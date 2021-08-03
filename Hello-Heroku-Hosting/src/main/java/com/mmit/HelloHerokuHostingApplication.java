6e13350977355cbee84f15220a91379bd873f044
NanEiTheint <naneieitheint2015@gmail.com>
2021-08-02 22:17:18 -0700

    heroku-hosting-test

---
+++ Hello-Heroku-Hosting/src/main/java/com/mmit/HelloHerokuHostingApplication.java
@@ -0,0 +1,13 @@
+package com.mmit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloHerokuHostingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloHerokuHostingApplication.class, args);
+	}
+
+}
